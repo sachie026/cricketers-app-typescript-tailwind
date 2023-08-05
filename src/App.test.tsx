56d037e6613e6aaff8b5eacf83695104f464c1e1
Sachin <jadhavsachin174@gmail.com>
2023-08-05 15:53:05 +0530

    Add details page unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
 import {
+  ALL_ROUNDER_LABEL,
   CRICKETERS_LIST_TITLE,
+  DETAILS_PAGE_TITLE,
   NEXT_LABEL,
   PREV_LABEL,
   SEARCH_PLACEHOLDER,
 } from "./data/labels";
-import { NAME_KEY, TYPE_ALL, TYPE_KEY } from "./data/shared";
+import { NAME_KEY, TYPE_KEY } from "./data/shared";
 
 test("renders the players list", async () => {
   render(<App />);
@@ -60,7 +62,7 @@ test("renders search filter bar", async () => {
   });
 
   await waitFor(() => {
-    const selectDropDown = screen.getByText(TYPE_ALL);
+    const selectDropDown = screen.getByText(ALL_ROUNDER_LABEL);
     expect(selectDropDown).toBeInTheDocument();
   });
 });
@@ -72,5 +74,31 @@ test("render and check the number of rows", async () => {
     const tableRow = screen.getAllByTestId("table-row");
 
     expect(tableRow).toHaveLength(10);
+  });
+});
+
+test("render and check next button click behavior", async () => {
+  render(<App />);
+
+  await waitFor(() => {
+    const nextbutton = screen.getByText(NEXT_LABEL);
+    // eslint-disable-next-line testing-library/no-wait-for-side-effects
+    fireEvent.click(nextbutton);
+
+    const pageLabel = screen.getByText("page 2 of 2");
+    expect(pageLabel).toBeInTheDocument();
+  });
+});
+
+test("render and check details page", async () => {
+  render(<App />);
+
+  await waitFor(() => {
+    const nameCol = screen.getAllByTestId("name-col");
+    // eslint-disable-next-line testing-library/no-wait-for-side-effects
+    fireEvent.click(nameCol[0]);
+
+    const detailsLabel = screen.getByText(DETAILS_PAGE_TITLE);
+    expect(detailsLabel).toBeInTheDocument();
   });
 });
